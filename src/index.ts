e835446231c08572edeaa3347f311c712561bc92
Julian Lawrance <julian.lawrance@gmail.com>
2021-07-01 21:46:21 +0100

    Create index file for exporting types

---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import { AwardCardDeck } from "./game/AwardCardDeck"
+import { Deck } from "./game/Deck"
+import { GreenCardDeck } from "./game/GreenCardDeck"
+import { Grid } from "./game/Grid"
+import { Pile } from "./game/Pile"
+import { VillageCardDeck } from "./game/VillageCardDeck"
+
+import { AwardCard } from "./game/cards/AwardCard"
+import { Card } from "./game/cards/Card"
+import { Colour } from "./game/cards/Colour"
+import { Feature } from "./game/cards/Feature"
+import { FeatureCard } from "./game/cards/FeatureCard"
+import { Flower } from "./game/cards/Flower"
+import { FlowerCard } from "./game/cards/FlowerCard"
+import { GreenCard } from "./game/cards/GreenCard"
+import { LawnCard } from "./game/cards/LawnCard"
+import { Tree } from "./game/cards/Tree"
+import { TreeCard } from "./game/cards/TreeCard"
+import { VillageCard } from "./game/cards/VillageCard"
+
+import { AmountOfFeature } from "./game/criteria/AmountOfFeature"
+import { AmountOfGroupsOfFeature } from "./game/criteria/AmountOfGroupsOfFeature"
+import { AmountOfGroupsOfTree } from "./game/criteria/AmountOfGroupsOfTree"
+import { AmountOfLawns } from "./game/criteria/AmountOfLawns"
+import { AmountOfUniqueTrees } from "./game/criteria/AmountOfUniqueTrees"
+import { EachCard } from "./game/criteria/EachCard"
+import { EachColour } from "./game/criteria/EachColour"
+import { EachEmpty } from "./game/criteria/EachEmpty"
+import { EachFeature } from "./game/criteria/EachFeature"
+import { EachFlower } from "./game/criteria/EachFlower"
+import { EachFlowerAndColour } from "./game/criteria/EachFlowerAndColour"
+import { EachLawn } from "./game/criteria/EachLawn"
+import { EachTree } from "./game/criteria/EachTree"
+import { FeatureVariety } from "./game/criteria/FeatureVariety"
+import { HasUniqueColours } from "./game/criteria/HasUniqueColours"
+import { HasUniqueFlowers } from "./game/criteria/HasUniqueFlowers"
+import { ICriterion } from "./game/criteria/ICriterion"
+import { NaturalAndManMade } from "./game/criteria/NaturalAndManMade"
+
+import { Random } from "./util/Random"
+import { allTrees } from "./util/Util"
+
+export {
+    AwardCardDeck,
+    Deck,
+    GreenCardDeck,
+    Grid,
+    Pile,
+    VillageCardDeck,
+
+    AwardCard,
+    Card,
+    Colour,
+    Feature,
+    FeatureCard,
+    Flower,
+    FlowerCard,
+    GreenCard,
+    LawnCard,
+    Tree,
+    TreeCard,
+    VillageCard,
+
+    AmountOfFeature,
+    AmountOfGroupsOfFeature,
+    AmountOfGroupsOfTree,
+    AmountOfLawns,
+    AmountOfUniqueTrees,
+    EachCard,
+    EachColour,
+    EachEmpty,
+    EachFeature,
+    EachFlower,
+    EachFlowerAndColour,
+    EachLawn,
+    EachTree,
+    FeatureVariety,
+    HasUniqueColours,
+    HasUniqueFlowers,
+    ICriterion,
+    NaturalAndManMade,
+
+    Random,
+    allTrees,
+}
